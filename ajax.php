c15165e498a156d545c7e113da134a1e77ed711f
Giovanni Giacobbi <giovanni@giacobbi.net>
2018-08-10 11:13:33 +0200

    final IHC 2018 demo version

--- ajax.php
+++ ajax.php
@@ -6,10 +6,15 @@ function do_transmit($data) {
     return false;
 
   if (flock($fd, LOCK_EX | LOCK_NB)) {
+    $logfd = fopen("log.log", "a");
+    fputs($logfd, "\n" . $data . "\n");
+    fclose($logfd);
+
     ftruncate($fp, 0);
-    fwrite($fd, data);
+    fwrite($fd, $data);
 
     $inv_data = invert_data($data);
+//var_dump($inv_data);
     //system("stty 156200 cs8 -parenb -cstopb ixon -F /dev/ttyUSB0");
     system("stty -F /dev/ttyUSB0 9600 -parenb");
     $usb = fopen("/dev/ttyUSB0", "w");
@@ -28,11 +33,49 @@ function do_transmit($data) {
   }
 }
 
+function from_binary($data) {
+  $retval = "";
+
+  $val = 0;
+  $xbit = 0;
+  for ($i = 0; $i < strlen($data); $i++) {
+    $ch = substr($data, $i, 1);
+    if ($ch == "1")
+      $val |= (1 << ($xbit));
+//    $val <<= 1;
+//    if ($ch == "1")
+//      $val |= 1;
+    $xbit++;
+    if ($xbit == 8) {
+      $retval .= sprintf("%02X", $val);
+      $xbit = 0;
+      $val = 0;
+    }
+  }
+
+  if ($val)
+    $retval .= sprintf("%02X", $val);
+
+  return $retval;
+}
+
 function action_publish() {
   $data = (isset($_POST['d']) ? (string) $_POST['d'] : null);
 
   if (preg_match('/^[a-fA-F0-9]{540}$/', $data)) {
     // @file_put_contents("data.bin", $data);
+    $retval = do_transmit($data);
+    if (!$retval) {
+      header("HTTP/ 400 Bad Request");
+      header("Content-Type: text/plain");
+      print "Error: Panel is busy\n";
+    }
+  }
+  elseif (preg_match('/^[01]{1,2160}$/', $data)) {
+//var_dump($data);
+    $data = from_binary($data);
+//var_dump($data); exit();
+//var_dump($data); exit();
     $retval = do_transmit($data);
     if (!$retval) {
       header("HTTP/ 400 Bad Request");
@@ -55,7 +98,7 @@ function action_acquire() {
 }
 
 function invert_data($data) {
-  if (!preg_match('/^[a-fA-F0-9]{540}$/', $data))
+  if (!preg_match('/^[a-fA-F0-9]{1,540}$/', $data))
     return false;
 
   $retval = "";
